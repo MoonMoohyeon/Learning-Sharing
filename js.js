b15041a6f6bec9e1be314a7c6463b79ac6146144
Kyprosa <propolis200@gmail.com>
2023-10-30 19:14:09 +0900

    10/30

---
+++ js.js
@@ -0,0 +1,31 @@
+function openModal1() {
+  modal1.style.display = "block";
+}
+
+function closeModal1() {
+  modal1.style.display = "none";
+}
+
+function openModal2() {
+  modal2.style.display = "block";
+}
+
+function closeModal2() {
+  modal2.style.display = "none";
+}
+
+function openModal3() {
+  modal3.style.display = "block";
+}
+
+function closeModal3() {
+  modal3.style.display = "none";
+}
+
+function openModal4() {
+  modal3.style.display = "block";
+}
+
+function closeModal4() {
+  modal3.style.display = "none";
+}
