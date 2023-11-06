7a6e8e3d5c0bd653244a4f49c6a058847f1cd3a3
Kyprosa <propolis200@gmail.com>
2023-11-06 19:49:48 +0900

    11/6

--- js.js
+++ js.js
@@ -29,3 +29,64 @@ function openModal4() {
 function closeModal4() {
   modal3.style.display = "none";
 }
+
+let state1 = true;
+let state2 = true;
+let state3 = true;
+let state4 = true;
+
+function spain() {
+  if (state1) {
+    const text = document.getElementById("spain");
+    text.innerHTML = "현재 597일째 학습 중";
+    text.style.color = "red";
+    state1 = false;
+  } else {
+    const text = document.getElementById("spain");
+    text.style.color = "black";
+    text.innerHTML = "스페인어 자격증 따기";
+    state1 = true;
+  }
+}
+
+function baekjun() {
+  if (state2) {
+    const text = document.getElementById("baekjun");
+    text.innerHTML = "현재 골드 3 1175점";
+    text.style.color = "red";
+    state2 = false;
+  } else {
+    const text = document.getElementById("baekjun");
+    text.style.color = "black";
+    text.innerHTML = "백준 플레찍기";
+    state2 = true;
+  }
+}
+
+function chess() {
+  if (state3) {
+    const text = document.getElementById("chess");
+    text.innerHTML = "레이팅 1800점 찍고 도전, 현재 1370점";
+    text.style.color = "red";
+    state3 = false;
+  } else {
+    const text = document.getElementById("chess");
+    text.style.color = "black";
+    text.innerHTML = "체스 레이팅 따기";
+    state3 = true;
+  }
+}
+
+function grade() {
+  if (state4) {
+    const text = document.getElementById("grade");
+    text.style.color = "red";
+    text.innerHTML = "현재 학점 평균 3.8";
+    state4 = false;
+  } else {
+    const text = document.getElementById("grade");
+    text.style.color = "black";
+    text.innerHTML = "학점 유지하며 졸업하기";
+    state4 = true;
+  }
+}
